7468d384681375ecefbef96924d0906fd690e8b3
윤창희 <chyun@yunchanghuiui-MacBookPro.local>
2018-12-08 22:50:44 +0900

    Model Add --> User, Image

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class TimeStampleModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+
+class Image(TimeStampleModel):
+    
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+class Comment(TimeStampleModel):
+
+    message = models.TextField()
+
